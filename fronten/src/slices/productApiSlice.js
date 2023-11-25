54e4907e729429c87aa3bdeaa23b0045c2384386
chinnnawat <chin3896@gmail.com>
2023-11-25 16:55:31 +0700

    ReviewFrontEnd

--- fronten/src/slices/productApiSlice.js
+++ fronten/src/slices/productApiSlice.js
@@ -48,7 +48,16 @@ export const productsApiSlice = apiSlice.injectEndpoints({
                 url: `${PRODUCTS_URL}/${productId}`,
                 method: 'DELETE'
             }),
-        })
+        }),
+
+        createReview: builder.mutation({
+            query: (data) => ({
+                url: `${PRODUCTS_URL}/${data.productId}/reviews`,
+                method: 'POST',
+                body: data,
+                }),
+                invalidatesTags: ['Product'],
+            }),
     })
 })
 export const { 
@@ -58,4 +67,5 @@ export const {
     useUpdateProductMutation,
     useUploadProductImageMutation,
     useDeleteProductMutation,
+    useCreateReviewMutation,
 } = productsApiSlice
